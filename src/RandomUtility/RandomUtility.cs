34d342d6049132f31e7f9e0ad0e411d10dead3fc
pavbsu <pavbsu@users.noreply.github.com>
2023-04-01 01:53:54 +0200

    GetInt method added

---
+++ src/RandomUtility/RandomUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace RandomUtility
+{
+    /// <summary>
+    /// Provides methods to generate pseudo-random objects.
+    /// </summary>
+    public static class RandomUtility
+    {
+        /// <summary>
+        /// Returns a non-negative random integer.
+        /// </summary>
+        /// <returns>
+        /// A 32-bit signed integer that is greater than or equal to 0 and less than <see cref="System.Int32.MaxValue"/>.
+        /// </returns>
+        public static int GetInt()
+        {
+            var random = new Random();
+
+            return random.Next();
+        }
+    }
+}
---
+++ test/RandomUtility.Tests/RandomUtilityTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace RandomUtility.Tests;
+
+public class RandomUtilityTests
+{
+    [Fact]
+    public void GetInt_Called100Times_AllResultsAreDifferent()
+    {
+        var results = new List<int>();
+
+        for (var _ = 0; _ < 100; _++)
+        {
+            results.Add(RandomUtility.GetInt());
+        }
+
+
+        results.Distinct().Should().HaveSameCount(results);
+    }
+}
